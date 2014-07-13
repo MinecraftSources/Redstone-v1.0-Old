1be3467d49b1e322c3df04f80a428868fcdb87b7
Jodie Belgrave <rmb1993@gmail.com>
2014-07-13 13:28:15 -0400

    added startup message

--- src/main/java/com/rmb938/mn2/docker/nc/NodeController.java
+++ src/main/java/com/rmb938/mn2/docker/nc/NodeController.java
@@ -1,14 +1,19 @@
 package com.rmb938.mn2.docker.nc;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class NodeController {
+
+    private static final Logger logger = LogManager.getLogger(NodeController.class.getName());
 
     public static void main(String[] args) {
         new NodeController();
     }
 
-
-
     public NodeController() {
+        logger.info("Started Node Controller");
+
         System.getenv("MONGO_URL");
         System.getenv("RABBITMQ_URL");
         System.getenv("REDIS_IP");
