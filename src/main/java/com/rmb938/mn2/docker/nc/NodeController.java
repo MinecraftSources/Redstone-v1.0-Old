a92e6b9f6a4aedcf50b9ecc9d8a03f2d6ac3d6a6
Jodie Belgrave <rmb1993@gmail.com>
2014-07-13 20:56:58 -0400

    setup mongo

--- src/main/java/com/rmb938/mn2/docker/nc/NodeController.java
+++ src/main/java/com/rmb938/mn2/docker/nc/NodeController.java
@@ -1,7 +1,12 @@
 package com.rmb938.mn2.docker.nc;
 
+import com.mongodb.MongoException;
+import com.mongodb.ServerAddress;
+import com.rmb938.mn2.docker.db.mongo.MongoDatabase;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
 
 public class NodeController {
 
@@ -11,11 +16,36 @@ public class NodeController {
         new NodeController();
     }
 
+    private MongoDatabase mongoDatabase;
+
     public NodeController() {
         logger.info("Started Node Controller");
 
-        System.getenv("MONGO_URL");
-        System.getenv("RABBITMQ_URL");
+        String mongoHosts = System.getenv("MONGO_HOSTS");
+        if (mongoHosts != null) {
+            String mongoDB = System.getenv("MONGO_DB");
+            if (mongoDB != null) {
+                ArrayList<ServerAddress> serverAddresses = new ArrayList<>();
+
+                try {
+                   mongoDatabase = new MongoDatabase(serverAddresses, mongoDB);
+                } catch (MongoException ex) {
+                    ex.printStackTrace();
+                    return;
+                }
+            } else {
+                logger.error("The MONGO_DB environment variable must be set");
+                return;
+            }
+        } else {
+            logger.error("The MONGO_HOSTS environment variable must be set");
+            return;
+        }
+
+        System.getenv("RABBITMQ_HOSTS");
+        System.getenv("RABBITMQ_USERNAME");
+        System.getenv("RABBITMQ_PASSWORD");
+
         System.getenv("REDIS_IP");
         System.getenv("REDIS_PORT");
     }
